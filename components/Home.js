14f061c578dd1515f08c44210bd96c8a8b40e906
Francesco Pazzaglia <francesco.pazzaglia2@gmail.com>
2023-10-09 13:15:08 +0200

    Inserimento del componente SelectDropdown, utilizzato per la selezione del bambino nella Home.  Impostato primo scheletro del metodo(getAssociations) per prendere le informazioni da db di ogni bambino associato ad un utente cucina.

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,60 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
 import { StyleSheet,  Text, View } from 'react-native';
+import SelectDropdown from 'react-native-select-dropdown';
 
 export default function Home({ route }) { 
+
+  //Variables
   const { value } = route.params;
+  const [associations, setAssociations] = useState([ ]);
+  const [getEffettuata, setGet] = useState(false);
+
+
+  //Methods
+  useEffect(()=>{
+    if(!getEffettuata){
+      getAssociations();
+    }
+  });
   
+  getAssociations = async() => {
+    try{
+      var data = new URLSearchParams();
+      data.append('username', value.username);
+      fetch('https://apis-pari-o-dispari.azurewebsites.net/getassociazionecucine', {
+          method: 'POST',
+          mode: 'cors',
+          headers: {
+          'Accept': 'application/x-www-form-urlencoded',
+          'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: data.toString(),
+          json:true,
+      }).then(response => response.json())
+      .then(response => {
+        setAssociations(response);
+      })
+    }catch(err)
+    {
+        console.log(err.message);
+    }
+  }
+
+
   return (
       <View style={styles.container}>
-            <Text style={{fontSize: 30, flex: 1, }}>Benvenuto/a {value.username}!</Text>
-
+            <Text style={{fontSize: 30, flex: 0.1, }}>Benvenuto/a {value.username}!</Text>
+            <View style = {{flex: 0.8}}>
+              <SelectDropdown 
+                data={associations}
+                defaultButtonText='Scegli il bambino'
+                search={true}
+                onSelect={(selectedItem, index) => {
+                  console.log(selectedItem, index)
+                }}
+              />
+              
+            </View>
       </View>
   );
 }
