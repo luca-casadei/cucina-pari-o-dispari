27f2fdc88e4c566ae736efbb471a40574c8af84d
Francesco Pazzaglia <francesco.pazzaglia3@studio.unibo.it>
2024-02-24 17:44:03 +0100

    Added Child and Menu info in chef home on select.

--- components/Home.js
+++ components/Home.js
@@ -8,13 +8,18 @@ export default function Home({ route }) {
   const { value } = route.params;
   const [associations, setAssociations] = useState([]);
   const [selectedChild, setSelectedChild] = useState(null);
+  const [kidMenuInfo, setKidMenuInfo] = useState(null);
+  const [menuDishes, setMenuDishes] = useState([]);
 
   // Metodi
   useEffect(() => {
     if (!associations.length) {
       getAssociations();
     }
-  }, []);
+    if (selectedChild && kidMenuInfo && kidMenuInfo.IdMenu) {
+      getKidMenuDishes(selectedChild.CodiceFiscaleBambino, kidMenuInfo.IdMenu);
+    }
+  }, [selectedChild, kidMenuInfo]);
 
   const getAssociations = async () => {
     try {
@@ -36,14 +41,67 @@ export default function Home({ route }) {
     }
   }
 
+  const getKidMenuInfo = async (codiceFiscaleParam) => {
+    try {
+      const response = await fetch('https://casadei.ddns.net:3000/getkidmenuinfo', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          codiceFiscale: codiceFiscaleParam,
+        })
+      });
+      const responseData = await response.text();
+      try {
+        const data = JSON.parse(responseData); 
+        setKidMenuInfo(data);
+      } catch (error) {
+        console.log("Errore nel parsing della risposta JSON:", error);
+        setKidMenuInfo(null);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };  
+
+  const getKidMenuDishes = async (codiceFiscaleParam, idMenu) => {
+    try {
+      const response = await fetch('https://casadei.ddns.net:3000/getkidmenudishes', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          codiceFiscale: codiceFiscaleParam,
+          idMenu: idMenu,
+        })
+      });
+      const data = await response.json();
+      setMenuDishes(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   const formatDate = (dateString) => {
     const date = new Date(dateString);
     return `${('0' + date.getDate()).slice(-2)}-${('0' + (date.getMonth() + 1)).slice(-2)}-${date.getFullYear()}`;
   };
 
   const handleSelectChild = (selectedItem, index) => {
-    setSelectedChild(associations[index]);
-  }
+    const selectedChild = associations[index];
+    if (selectedChild) {
+      setSelectedChild(selectedChild);
+      getKidMenuInfo(selectedChild.CodiceFiscaleBambino);
+    } else {
+      console.log("SelectedChild is null or undefined");
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -57,9 +115,27 @@ export default function Home({ route }) {
         />
         {selectedChild && (
           <View style={styles.childDetailsContainer}>
+            <Text style={styles.sectionHeader}>Informazioni:</Text>
             <Text style={styles.childDetailText}>Data di nascita: {formatDate(selectedChild.DataNascitaBambino)}</Text>
             <Text style={styles.childDetailText}>Codice fiscale: {selectedChild.CodiceFiscaleBambino}</Text>
             <Text style={styles.childDetailText}>Email tutore: {selectedChild.EmailBambino}</Text>
+            {kidMenuInfo && (
+              <View>
+                <Text style={styles.sectionHeader}>Menu:</Text>
+                <Text style={styles.menuDetailText}>Id Menu: {kidMenuInfo?.IdMenu}</Text>
+                <Text style={styles.menuDetailText}>Nome Menu: {kidMenuInfo?.NomeMenu}</Text>
+                <Text style={styles.menuDetailText}>Stagione: {kidMenuInfo?.StagioneMenu}</Text>
+                <Text style={styles.menuDetailText}>Email creatore: {kidMenuInfo?.EmailCreatoreMenu}</Text>
+                {menuDishes.length > 0 && (
+                  <>
+                    <Text style={styles.sectionHeader}>Piatti:</Text>
+                    {menuDishes.map((item, index) => (
+                      <Text key={index} style={styles.menuItem}>{item.NomePiatto} - {item.Descrizione}</Text>
+                    ))}
+                  </>
+                )}
+              </View>
+            )}
           </View>
         )}
       </View>
@@ -87,5 +163,18 @@ const styles = StyleSheet.create({
   childDetailText: {
     fontSize: 18,
     marginBottom: 10,
+  },
+  sectionHeader: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  menuDetailText: {
+    fontSize: 18,
+    marginBottom: 5,
+  },
+  menuItem: {
+    fontSize: 16,
+    marginLeft: 20,
   },
 });
